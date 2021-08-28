74e1385cff1f060957a4eac9629c3aa24f509b89
Matcha_sama <mingzuoma@gmail.com>
2021-08-28 09:25:18 +0800

    Update hentaiathome.ts

--- src/services/ui-translation/data/hentaiathome.ts
+++ src/services/ui-translation/data/hentaiathome.ts
@@ -5,6 +5,20 @@ merge(
     undefined,
     {
         'Hentai@Home Clients': 'Hentai@Home 客户端',
+        'H@H Region': 'H@H 地区',
+        'Current Network Load': '当前网络负载',
+        'H@H Miss%': 'H@H 丢包率%',
+        'Coverage': '覆盖范围',
+        'Hits/GB': '命中/GB',
+        'Quality': '质量',
+        'North and South America': '美洲',
+        'Europe and Africa': '欧洲和非洲',
+        'Asia and Oceania': '亚洲和大洋洲',
+        'Global': '全球',
+        'Current Network Load shows how much raw bandwidth is currently used to serve images.': '当前网络负载显示当前用于提供图像的原始带宽量。'
+        ' This includes requests served by H@H as well as direct requests from the image servers.': '这包括 H@H 提供的请求以及来自图像服务器的直接请求。'
+        'H@H Miss% shows the percentage of requests for the region that would have gone to a H@H client if one was available, but where no client was ready to serve the request.': 'H@H 丢包率% 显示该区域的请求将发送到 H@H 客户端（如果一个客户端可用，但没有客户端准备为该请求提供服务）的百分比。'
+        ''
     },
     [],
 );
